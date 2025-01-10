f1302ec9b0e2ac2cf6b1fa6a05a44b6fc7790aa9
Amir Ehsandar <ehsandaramir@gmail.com>
2025-01-10 19:53:12 +0330

    [Refactor] Move commonly used code to routes.go and generalize errors

--- internal/tiles/gettile.go
+++ internal/tiles/gettile.go
@@ -1,9 +1,7 @@
 package tiles
 
 import (
-	"encoding/json"
-	"log/slog"
-	"net/http"
+	"context"
 
 	"github.com/loreanvictor/baldosa.git/internal/storage"
 )
@@ -16,48 +14,7 @@ type getTileResponse struct {
 	Tile storage.Tile `json:"tile"`
 }
 
-func (s *tilesServer) GetTile(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	ctx := r.Context()
-	var err error
-
-	request := getTileRequest{}
-	err = json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		slog.InfoContext(ctx, "invalid request body")
-		http.Error(w, "invalid request body", http.StatusBadRequest)
-		return
-	}
-
+func (s *tilesServer) GetTile(ctx context.Context, request getTileRequest) (getTileResponse, error) {
 	tile, err := s.querier.GetTileByID(ctx, s.pool, request.ID)
-	if err != nil {
-		slog.ErrorContext(
-			ctx, "failed to get tile",
-			"error", err,
-			"request", request,
-		)
-
-		code, storageErr := storage.TransformPgxError(err)
-		if storageErr != nil {
-			http.Error(w, storageErr.Error(), code)
-		} else {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		return
-	}
-
-	response := getTileResponse{Tile: tile}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
-		slog.ErrorContext(ctx, "failed to encode response", "error", err)
-		http.Error(w, "failed to encode response", http.StatusInternalServerError)
-	}
+	return getTileResponse{Tile: tile}, err
 }
--- internal/tiles/routes.go
+++ internal/tiles/routes.go
@@ -1,6 +1,8 @@
 package tiles
 
 import (
+	"encoding/json"
+	"log/slog"
 	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -22,6 +24,70 @@ func RegisterServer(
 		querier: querier,
 	}
 
-	mux.HandleFunc("/tiles/GetTile", s.GetTile)
-	mux.HandleFunc("/tiles/CreateTile", s.CreateTile)
+	mux.HandleFunc("/tiles/{rpc}", s.handleRequest)
+}
+
+func (s *tilesServer) handleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	rpc := r.PathValue("rpc")
+
+	ctx := r.Context()
+	var err error
+	var response any
+
+	switch rpc {
+	case "GetTile":
+		request := getTileRequest{}
+		err = json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, "invalid request body", http.StatusBadRequest)
+			return
+		}
+		response, err = s.GetTile(ctx, request)
+	case "CreateTile":
+		request := createTileRequest{}
+		err = json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			http.Error(w, "invalid request body", http.StatusBadRequest)
+			return
+		}
+		response, err = s.CreateTile(ctx, request)
+	default:
+		http.Error(w, "invalid rpc", http.StatusNotFound)
+	}
+
+	if err != nil {
+		slog.ErrorContext(
+			ctx, "RPC failed",
+			"rpc", rpc,
+			"error", err,
+		)
+
+		code, storageErr := storage.TransformPgxError(err)
+		if storageErr != nil {
+			http.Error(w, storageErr.Error(), code)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		slog.ErrorContext(
+			ctx, "failed to encode response",
+			"error", err,
+			"rpc", rpc,
+			"response", response,
+		)
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
