ac5c656a1dac609fde13780b17aa0924571ca8ad
Amir Ehsandar <ehsandaramir@gmail.com>
2025-01-10 03:51:51 +0330

    Add config and migrations

---
+++ cmd/config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Database DatabaseConfig `mapstructure:"database"`
+}
+
+type DatabaseConfig struct {
+	ConnString string `mapstructure:"conn_string"`
+}
+
+func loadConfig() Config {
+	var config Config
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
+	viper.SetEnvPrefix("BALDOSA")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Failed to read config: %v", err)
+	}
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatalf("Failed to unmarshal config: %v", err)
+	}
+
+	return config
+}
