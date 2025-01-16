7ec31b7ec821f69eca1722306a844d28769e7d08
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2025-01-16 16:53:41 +0100

    add color response for each published image

--- publisher/src/image/publish.rs
+++ publisher/src/image/publish.rs
@@ -1,8 +1,8 @@
 use serde::Serialize;
-use std::{ sync::Arc, collections::HashMap, error::Error };
+use std::{ sync::Arc, collections::HashMap, error::Error, sync::Mutex as MX };
 use tokio::{ task::{ spawn, spawn_blocking }, sync::Mutex };
 use futures::{ future::join_all, executor::block_on };
-use image::{ Pixel, imageops::{ resize, fast_blur, FilterType }};
+use image::{ Rgb, Pixel, imageops::{ resize, fast_blur, FilterType }};
 
 use super::util::crop_to_square;
 use super::io::interface::{ ImageInterface, Metadata };
@@ -10,7 +10,9 @@ use super::super::config::Config;
 
 
 #[derive(Serialize, Debug)]
-pub struct PublishResult {
+pub struct PublishResult<P: Pixel>
+  where P::Subpixel: Serialize {
+  pub color: Option<Vec<P::Subpixel>>,
   pub images: HashMap<u32, String>,
 }
 
@@ -23,8 +25,8 @@ pub async fn publish<P: Pixel + Send + Sync + 'static> (
   link: &str,
   io: Arc<dyn ImageInterface<Pixel = P>>,
   config: Arc<Config>,
-) -> Result<PublishResult, Box<dyn Error + Send + Sync>>
-  where P::Subpixel: Send + Sync {
+) -> Result<PublishResult<P>, Box<dyn Error + Send + Sync>>
+  where P::Subpixel: Send + Sync + Serialize {
   let image = io.load(&source).await?;
   let square = Arc::new(crop_to_square(&image));
   let published_shared = Arc::new(Mutex::new(HashMap::<u32, String>::new()));
@@ -33,6 +35,8 @@ pub async fn publish<P: Pixel + Send + Sync + 'static> (
     subtitle: subtitle.to_string(),
     link: link.to_string(),
   };
+
+  let color: Arc<MX<Option<Rgb<P::Subpixel>>>> = Arc::new(MX::new(None));
 
   let mut tasks: Vec<_> = config.sizes.iter().map(|size| {
     let square = Arc::clone(&square);
@@ -46,10 +50,17 @@ pub async fn publish<P: Pixel + Send + Sync + 'static> (
     let io = Arc::clone(&io);
     let meta = meta.clone();
 
+    let color = Arc::clone(&color);
+
     spawn_blocking(move || {
       let resized = resize(&*square, size, size, FilterType::Lanczos3);
       let blurred = if blur_amount > 0.0 { fast_blur(&resized, blur_amount) } else { resized };
       let saved = block_on(io.save(&blurred, &meta, &target)).unwrap();
+
+      if size == 1 {
+        let mut color = color.lock().unwrap();
+        *color = Some(blurred.get_pixel(0, 0).to_rgb());
+      }
 
       let mut published = published.blocking_lock();
       published.insert(size, saved);
@@ -70,8 +81,14 @@ pub async fn publish<P: Pixel + Send + Sync + 'static> (
   // TODO: better error handling here
   join_all(tasks).await;
 
+  let color = match &*color.lock().unwrap() {
+    Some(color) => Some(vec![color.0[0], color.0[1], color.0[2]]),
+    None => None,
+  };
+
   let published = published_shared.lock().await;
   Ok(PublishResult {
+    color: color,
     images: published.clone(),
   })
 }
