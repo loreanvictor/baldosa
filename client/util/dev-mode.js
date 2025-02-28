9252c11a9efaab2d4f8c0b647a5ca06479fcf874
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2025-02-28 23:30:42 +0100

    cooler dev mode goodies

--- client/util/dev-mode.js
+++ client/util/dev-mode.js
@@ -1,5 +1,8 @@
 import { parse } from 'https://esm.sh/envfile'
-import { define } from 'https://esm.sh/minicomp'
+import { define, attachControls } from 'https://esm.sh/minicomp'
+import { ref, html } from 'https://esm.sh/rehtm'
+
+import './glass-modal.js'
 
 
 let devModeAlert = false
@@ -9,9 +12,10 @@ export const isDevMode = () => {
 
   if (devMode && !devModeAlert) {
     devModeAlert = true
-    console.log('%cBALDOSA DEV MODE IS ON', 'color: #EFA73E; font-weight: bold; font-size: 1.1rem')
-    console.log('run %csetDevMode(false)', 'background: black; padding: .5rem; border-radius: 3px',
-                'to disable dev mode.')
+    console.log('%c-= BALDOSA DEV MODE =-', 'color: #EFA73E; font-weight: bold; font-size: 1.1rem')
+    console.log('run %csetDevMode%c(%cfalse%c) %cin the console to disable dev mode',
+        'color: #9e9e9e', 'color: #48A1D8', 'color: #EFA73E', 'color: #48A1D8', 'color: inherit'
+    )
 
     const $ind = document.querySelector('dev-mode-indicator')
     $ind.style.display = 'block'
@@ -22,15 +26,22 @@ export const isDevMode = () => {
   return devMode
 }
 
-export const setDevMode = (value) => {
+export const setDevMode = (value, reload = true) => {
   localStorage.setItem('--dev-mode', value)
-  window.location.reload()
+  reload && window.location.reload()
 }
 
 export const loadDevEnv = async () => {
-  const envfile = await fetch('/.env')
-  const text = await envfile.text()
+  const response = await fetch('/.env')
+  if (!response.ok) {
+    console.warn('no .env file found')
+    return {}
+  }
+
+  const text = await response.text()
   const env = parse(text)
+
+  document.querySelector('dev-mode-indicator')?.controls?.setEnv(env)
 
   console.log('loaded dev env:')
   Object.entries(env).forEach(([key, value]) => {
@@ -42,7 +53,18 @@ export const loadDevEnv = async () => {
 
 
 define('dev-mode-indicator', () => {
-  return `
+  const modal = ref()
+  const env = ref()
+
+  attachControls({
+    setEnv: vars => {
+      env.current.innerHTML = Object.entries(vars).map(([key, value]) =>
+        `<li><label>${key}</label> = ${value}</li>`
+      ).join('')
+    }
+  })
+
+  return html`
     <style>
     :host {
       display: none;
@@ -55,10 +77,47 @@ define('dev-mode-indicator', () => {
       padding: 1ch;
       border-top-right-radius: 12px;
       font-size: 0.8rem;
+      cursor: pointer;
+    }
+    code {
+      padding: .5ch;
+      border-radius: .5ch;
+      margin: .5ch;
+      [fn] { color: #9e9e9e; }
+      [par] { color: #48A1D8; }
+      [val] { color: #EFA73E; }
+    }
+    ul {
+      list-style: none;
+      padding: 1ch;
+      margin: 0;
+
+      li {
+        color: #48A1D8;
+        label {
+          color: #447ED3;
+          font-weight: bold;
+        }
+      }
     }
     </style>
-    <glass-pane>
-      <span>developer mode</span>
+    <glass-pane onclick=${() => modal.current.controls.open()}>
+      <span>dev mode</span>
     </glass-pane>
+    <glass-modal ref=${modal}>
+      <span slot="title">Developer Mode</span>
+      <div>
+        <p>
+          Developer mode is enabled. You can disable it by running
+          <code><span fn>setDevMode</span><span par>(</span><span val>false</span><span par>)</span></code>
+          in the console.
+        </p>
+        <p>
+          Defined dev env:
+          <ul ref=${env}>
+          </ul>
+        </p>
+      </div>
+    </glass-modal>
   `
 })
