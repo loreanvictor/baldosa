57d6d7fdc18d4a2881acc472a4e9d2352c08c1fd
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2025-11-25 15:12:41 +0100

    fix base64 decoding to utf8

--- client/grid/render/image/fetch-worker.js
+++ client/grid/render/image/fetch-worker.js
@@ -1,7 +1,6 @@
-import createError from 'https://esm.run/http-errors@2'
+import createError from "https://esm.run/http-errors@2"
 
-
-const parseDetails = encoded => {
+const parseDetails = (encoded) => {
   try {
     return JSON.parse(atob(encoded))
   } catch (error) {
@@ -9,24 +8,29 @@ const parseDetails = encoded => {
   }
 }
 
+const decodeBase64 = (str) => {
+  const bytes = Uint8Array.from(atob(str), (c) => c.charCodeAt(0))
+  return new TextDecoder().decode(bytes)
+}
+
 self.onmessage = async ({ data }) => {
   const { url, reload } = data
 
   const err = (error) => {
-    console.error('error in loading image ' + url)
+    console.error("error in loading image " + url)
     console.error(error)
     self.postMessage({ url, error: error.toString() })
   }
 
   try {
-    const response = reload ? await fetch(url, { cache: 'reload' }) : await fetch(url)
+    const response = reload ? await fetch(url, { cache: "reload" }) : await fetch(url)
     if (response.ok) {
       const meta = {
-        title: response.headers.get('x-amz-meta-title'),
-        subtitle: response.headers.get('x-amz-meta-subtitle'),
-        description: atob(response.headers.get('x-amz-meta-description') ?? ''),
-        link: response.headers.get('x-amz-meta-link'),
-        details: parseDetails(response.headers.get('x-amz-meta-details') ?? ''),
+        title: response.headers.get("x-amz-meta-title"),
+        subtitle: response.headers.get("x-amz-meta-subtitle"),
+        description: decodeBase64(response.headers.get("x-amz-meta-description") ?? ""),
+        link: response.headers.get("x-amz-meta-link"),
+        details: parseDetails(response.headers.get("x-amz-meta-details") ?? ""),
       }
       const blob = await response.blob()
       const bitmap = await createImageBitmap(blob)
