5cb781a7e7112cedf92d65ebb6421cf987a33c9a
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2025-11-26 13:03:05 +0100

    add support for mime encoded title and subtitles

--- client/grid/render/image/fetch-worker.js
+++ client/grid/render/image/fetch-worker.js
@@ -1,6 +1,6 @@
 import createError from 'https://esm.run/http-errors@2'
 
-const parseDetails = (encoded) => {
+const decodeDetails = (encoded) => {
   try {
     return JSON.parse(atob(encoded))
   } catch (error) {
@@ -11,6 +11,27 @@ const parseDetails = (encoded) => {
 const decodeBase64 = (str) => {
   const bytes = Uint8Array.from(atob(str), (c) => c.charCodeAt(0))
   return new TextDecoder().decode(bytes)
+}
+
+const decodeMIME = (str) => {
+  if (!str) return str
+
+  const m = str.match(/^=\?([^?]+)\?Q\?([^?]+)\?=$/i)
+  if (!m) return str // not MIME → return unchanged
+
+  const [, charset, encoded] = m
+
+  const qp = encoded.replace(/_/g, ' ').replace(/=([A-F0-9]{2})/gi, (_, h) => String.fromCharCode(parseInt(h, 16)))
+  const bytes = new Uint8Array([...qp].map((c) => c.charCodeAt(0)))
+  const demimed = new TextDecoder(charset).decode(bytes)
+
+  try {
+    // sometimes it is double encoded, so double decode
+    const bytes = Uint8Array.from(demimed, (c) => c.charCodeAt(0))
+    return new TextDecoder(charset).decode(bytes, { fatal: true })
+  } catch (err) {
+    return demimed
+  }
 }
 
 self.onmessage = async ({ data }) => {
@@ -26,11 +47,11 @@ self.onmessage = async ({ data }) => {
     const response = reload ? await fetch(url, { cache: 'reload' }) : await fetch(url)
     if (response.ok) {
       const meta = {
-        title: response.headers.get('x-amz-meta-title'),
-        subtitle: response.headers.get('x-amz-meta-subtitle'),
+        title: decodeMIME(response.headers.get('x-amz-meta-title')),
+        subtitle: decodeMIME(response.headers.get('x-amz-meta-subtitle')),
         description: decodeBase64(response.headers.get('x-amz-meta-description') ?? ''),
         link: response.headers.get('x-amz-meta-link'),
-        details: parseDetails(response.headers.get('x-amz-meta-details') ?? ''),
+        details: decodeDetails(response.headers.get('x-amz-meta-details') ?? ''),
       }
       const blob = await response.blob()
       const bitmap = await createImageBitmap(blob)
