0a82badc7200f3f3b88f069b4f0816eb78518e48
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2025-12-04 19:17:41 +0100

    properly annotate occupant bid cols as optional for fetching pending bids

--- bank/src/bidding/book/user.rs
+++ bank/src/bidding/book/user.rs
@@ -1,5 +1,8 @@
-use anyhow::Context;
-use sqlx::types::Uuid;
+use anyhow::{anyhow, Context};
+use sqlx::types::{
+  chrono::{DateTime, Utc},
+  JsonValue, Uuid,
+};
 
 use super::bid::{Bid, BidContent, PendingBid};
 use super::core::Book;
@@ -40,7 +43,9 @@ impl Book {
       r#"
         select
           bids.id, bids.x, bids.y, bids.amount, bids.tx, bids.created_at, bids.content, bids.bidder,
-          ocb.id as "o_id?: Uuid", ocb.amount as o_amount, ocb.created_at as o_created_at, ocb.content as o_content, ocb.published_at as o_published_at, ocb.bidder as o_bidder from bids
+          ocb.id as "o_id?: Uuid", ocb.amount as "o_amount?: i32", ocb.created_at as "o_created_at?: DateTime<Utc>",
+          ocb.content as "o_content?: JsonValue", ocb.published_at as "o_published_at?: DateTime<Utc>",
+          ocb.bidder as "o_bidder?: Uuid" from bids
         left join published_tiles on bids.x = published_tiles.x and bids.y = published_tiles.y
         left join bids ocb on published_tiles.occupant_bid = ocb.id
         where bids.bidder = $1 and bids.published_at is null and bids.rejection is null
@@ -58,34 +63,52 @@ impl Book {
     Ok(
       rows
         .iter()
-        .map(|row| PendingBid {
-          bid: Bid {
-            id: row.id,
-            x: row.x,
-            y: row.y,
-            bidder: row.bidder,
-            amount: row.amount,
-            tx: row.tx,
-            created_at: row.created_at,
-            content: BidContent::from(row.content.clone()),
-            published_at: None,
-            rejection: None,
-          },
-          occupant: row.o_id.map(|id| Bid {
-            id,
-            x: row.x,
-            y: row.y,
-            bidder: row.o_bidder,
-            amount: row.o_amount,
-            tx: row.tx,
-            created_at: row.o_created_at,
-            content: BidContent::from(row.o_content.clone()),
-            published_at: row.o_published_at,
-            rejection: None,
-          }),
-          next_auction: None,
+        .map(|row| -> Result<PendingBid, anyhow::Error> {
+          let occupant =
+            row
+              .o_id
+              .map(|id| -> Result<Bid, anyhow::Error> {
+                Ok(Bid {
+                  id,
+                  x: row.x,
+                  y: row.y,
+                  bidder: row
+                    .o_bidder
+                    .ok_or_else(|| anyhow!("missing occupant bidder when fetching pending bid"))?,
+                  amount: row
+                    .o_amount
+                    .ok_or_else(|| anyhow!("missing occupant amount when fetching pending bid"))?,
+                  tx: row.tx,
+                  created_at: row.o_created_at.ok_or_else(|| {
+                    anyhow!("missing occupant created_at when fetching pending bid")
+                  })?,
+                  content: BidContent::from(row.o_content.clone().ok_or_else(|| {
+                    anyhow!("missing occupant content when fetching pending bid")
+                  })?),
+                  published_at: row.o_published_at,
+                  rejection: None,
+                })
+              })
+              .transpose()?;
+
+          Ok(PendingBid {
+            bid: Bid {
+              id: row.id,
+              x: row.x,
+              y: row.y,
+              bidder: row.bidder,
+              amount: row.amount,
+              tx: row.tx,
+              created_at: row.created_at,
+              content: BidContent::from(row.content.clone()),
+              published_at: None,
+              rejection: None,
+            },
+            occupant,
+            next_auction: None,
+          })
         })
-        .collect(),
+        .collect::<Result<Vec<_>, _>>()?,
     )
   }
 
