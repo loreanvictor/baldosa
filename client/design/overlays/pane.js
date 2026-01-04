a8e8e20be82d54f9b4542e562634b51b6d933c9a
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2026-01-04 23:43:17 +0100

    visual retuning

--- client/design/overlays/pane.js
+++ client/design/overlays/pane.js
@@ -5,10 +5,11 @@ define(
   () => `
   <style>
     :host {
-      background: #42424244;
-      backdrop-filter: blur(6px) contrast(1.2);
-      -webkit-backdrop-filter: blur(6px) contrast(1.2);
-      box-shadow: 0 0 1px 1px #ffffff22;
+      background: linear-gradient(180deg, #ffffff22, #ffffff19);
+      backdrop-filter: blur(4px) contrast(1.5) brightness(1.1);
+      -webkit-backdrop-filter: blur(4px) contrast(1.5) brightness(1.1);
+      box-shadow: 0 0 2px 1px #ffffff22,
+        0 2px 8px 0 #00000022;
       color: #ffffff88;
     }
   </style>
